f4c5600c7f82af8428efef26ee8859911db0bd03
@kishorspark2120 <kishorekumar@sparkouttech.com>
2021-07-15 10:18:22 +0530

    changes

--- src/worker2.js
+++
@@ -1,7 +0,0 @@
-let cities = [];
-this.onmessage = ({data})=>{
-  data.map(d=>d.states).forEach(data=>{
-    data.map(d=>d.cities).map(d=>d.map(c=>cities.push(c.name)))
-  })
-  this.postMessage(cities)
-}
